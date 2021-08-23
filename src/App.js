58196460843c355f7bf93a3607732df8fd152dfb
walid <boulima.walid12@gmail.com>
2021-08-23 01:39:20 +0100

    jsxcheckpoint

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import imageSRC from './imageInSrc.jpg';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+         <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+
+            <h1 className="title red">Your name here</h1>
+     
+         <br/>
+
+         <img src={imageSRC}/>
+
+          <br/>
+
+          <img src="/imageInPublic.jpg" />
+
+       </div>
+
+     <video width={320} height={240} controls>
+
+          <source src="myVideo.mp4" type="video/mp4" />
+
+           </video>
+    </div>
+    
+  
+  );
+}
+
+export default App;
